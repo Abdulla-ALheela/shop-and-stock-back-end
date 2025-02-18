da437f2f4c2bef85e7ca737ded6b66bf7c984347
hamdan.ga <hamdanchitari@gmail.com>
2025-02-18 20:17:49 +0300

    completed

--- controllers/lists.js
+++ controllers/lists.js
@@ -18,7 +18,7 @@ router.post("/", verifyToken, async (req, res) => {
 
 
 //get all lists
-router.get("/", verifyToken, async (req, res) => {
+router.get("/", async (req, res) => {
     try {
       const lists = await List.find({}).populate("owner")
       res.status(200).json(lists);
@@ -27,29 +27,23 @@ router.get("/", verifyToken, async (req, res) => {
     }
   });
   
+
 // Add item to a list (POST)
-router.post("/:listId/items", verifyToken, async (req, res) => {
+router.post('/:listId/items',verifyToken,  async (req, res) => {
   try {
+    req.body.owner = req.user._id;
     const list = await List.findById(req.params.listId);
     if (!list) {
-      return res.status(404).json({ msg: "List not found" });
+      return res.status(404).json({ msg: 'List not found' });
     }
+    if (!list.items) list.items = [];
 
-    // Check if the user is the owner of the list
-    if (list.owner.toString() !== req.user._id.toString()) {
-      return res.status(403).json({ msg: "You are not authorized to add items to this list" });
-    }
-
-    // Create new item and add it to the list
-    const newItem = {
-      name: req.body.name,
-      quantity: req.body.quantity,
-      unit: req.body.unit,
-      isPurchased: req.body.isPurchased || false,
-    };
-
-    list.items.push(newItem);
+    list.items.push(req.body);
     await list.save();
+
+    // Find the newly added item:
+    const newItem = list.items[list.items.length - 1];
+    newItem._doc.addedBy = req.user;
 
     res.status(201).json(newItem);
   } catch (err) {
@@ -58,4 +52,66 @@ router.post("/:listId/items", verifyToken, async (req, res) => {
 });
 
 
-module.exports = router
+// GET list by ID (GET)
+router.get("/:listId", verifyToken, async (req, res) => {
+  try {
+    const list = await List.findById(req.params.listId).populate('owner');
+    if (!list) {
+      return res.status(404).json({ msg: "List not found" });
+    }
+    res.status(200).json(list);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+ //Get an item from a list (GET)
+router.get("/:listId/items/:itemId", verifyToken, async (req, res) => {
+  try {
+    const list = await List.findById(req.params.listId).populate('owner');
+    if (!list) {
+      return res.status(404).json({ msg: "List not found" });
+    }
+
+    const item = list.items.id(req.params.itemId);
+    if (!item) {
+      return res.status(404).json({ msg: "Item not found" });
+    }
+    
+    res.status(200).json(item);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+//lists.js
+
+router.put('/:listId', verifyToken, async (req, res) => {
+  try {
+    // Find the list:
+    const list = await List.findById(req.params.listId);
+
+   
+     if (!list.owner.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+  
+    const updatedList = await List.findByIdAndUpdate(
+      req.params.listId,
+      req.body,
+      { new: true }
+    );
+
+    // Append req.user to the owner property:
+    updatedList._doc.owner = req.user;
+
+    // Issue JSON response:
+    res.status(200).json(updatedList);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+    
+module.exports = router;
