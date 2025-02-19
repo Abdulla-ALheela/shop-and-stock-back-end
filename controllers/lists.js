92d7a6bf91e34c453f6e35db90b3713cb173e52c
Abdulla-Alheela <abdulla.alheela@gmail.com>
2025-02-19 13:41:26 +0000

    delet conflect

--- controllers/lists.js
+++ controllers/lists.js
@@ -2,30 +2,6 @@ const express = require('express')
 const router = express.Router()
 const List = require('../models/List')
 const verifyToken = require('../middleware/verify-token')
-
-
-//Create list 
-router.post("/", verifyToken, async (req, res) => {
-    try {
-      req.body.owner = req.user._id;
-      const list = await List.create(req.body);
-      list._doc.owner = req.user;
-      res.status(201).json(list);
-    } catch (err) {
-      res.status(500).json({ err: err.message });
-    }
-  });
-
-
-//get all lists
-router.get("/", verifyToken,async (req, res) => {
-    try {
-      const lists = await List.find({}).populate("owner")
-      res.status(200).json(lists);
-    } catch (err) {
-      res.status(500).json({ err: err.message });
-    }
-  });
   
 
 // Add item to a list (POST)
@@ -50,39 +26,7 @@ router.post('/:listId/items',verifyToken,  async (req, res) => {
     res.status(500).json({ err: err.message });
   }
 });
-
-
-// GET list by ID (GET)
-router.get("/:listId", verifyToken, async (req, res) => {
-  try {
-    const list = await List.findById(req.params.listId).populate('owner');
-    if (!list) {
-      return res.status(404).json({ msg: "List not found" });
-    }
-    res.status(200).json(list);
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
-
- //Get an item from a list (GET)
-router.get("/:listId/items/:itemId", verifyToken, async (req, res) => {
-  try {
-    const list = await List.findById(req.params.listId).populate('owner');
-    if (!list) {
-      return res.status(404).json({ msg: "List not found" });
-    }
-
-    const item = list.items.id(req.params.itemId);
-    if (!item) {
-      return res.status(404).json({ msg: "Item not found" });
-    }
-    
-    res.status(200).json(item);
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
+ 
 
 //lists.js
 
