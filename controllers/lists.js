d0a6d28b7cdee1ab876fb728b5714ab38f301ff7
Abdulla-Alheela <abdulla.alheela@gmail.com>
2025-02-19 13:53:47 +0000

    delet conflect

--- controllers/lists.js
+++ controllers/lists.js
@@ -58,4 +58,28 @@ router.put('/:listId', verifyToken, async (req, res) => {
     res.status(500).json({ err: err.message });
   }
 });    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
