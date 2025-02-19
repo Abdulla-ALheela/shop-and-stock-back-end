59e7197fb0fa59551ad5b3fb8534e67b4210df6f
Abdulla-Alheela <abdulla.alheela@gmail.com>
2025-02-19 14:17:53 +0000

    added edit list

--- controllers/lists.js
+++ controllers/lists.js
@@ -26,4 +26,31 @@ router.post('/:listId/items',verifyToken,  async (req, res) => {
   }
 });
  
+router.put('/:listId', verifyToken, async (req, res) => {
+  try {
+    // Find the list:
+    const list = await List.findById(req.params.listId);
+
+   
+    if (!list.owner.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+  
+    const updatedList = await List.findByIdAndUpdate(
+      req.params.listId,
+      req.body,
+      { new: true }
+    );
+
+    // Append req.user to the owner property:
+    updatedList._doc.owner = req.user;
+
+    // Issue JSON response:
+    res.status(200).json(updatedList);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
 module.exports = router;
