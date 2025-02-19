0093fe63a4e22ce6c9fd7f9f19e5a1e1859191b5
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-02-19 17:21:06 +0300

    Hamdan Code

--- controllers/lists.js
+++ controllers/lists.js
@@ -25,6 +25,57 @@ router.get("/", verifyToken, async (req, res) => {
   }
 });
 
+router.put('/:listId', verifyToken, async (req, res) => {
+  try {
+    // Find the list:
+    const list = await List.findById(req.params.listId);
+
+   
+    if (!list.owner.equals(req.user._id)) {
+      return res.status(403).send("You're not allowed to do that!");
+    }
+
+  
+    const updatedList = await List.findByIdAndUpdate(
+      req.params.listId,
+      req.body,
+      { new: true }
+    );
+
+    // Append req.user to the owner property:
+    updatedList._doc.owner = req.user;
+
+    // Issue JSON response:
+    res.status(200).json(updatedList);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// Add item to a list (POST)
+router.post('/:listId/items',verifyToken,  async (req, res) => {
+  try {
+    req.body.owner = req.user._id;
+    const list = await List.findById(req.params.listId);
+    if (!list) {
+      return res.status(404).json({ msg: 'List not found' });
+    }
+    if (!list.items) list.items = [];
+
+    list.items.push(req.body);
+    await list.save();
+
+    // Find the newly added item:
+    const newItem = list.items[list.items.length - 1];
+    newItem._doc.addedBy = req.user;
+
+    res.status(201).json(newItem);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+
 // Edit item in a list (PUT)
 router.put("/:listId/items/:itemId", verifyToken, async (req, res) => {
   try {
@@ -114,19 +165,6 @@ router.put("/:listId", verifyToken, async (req, res) => {
   }
 
 });
-
-
-//SHOW LIST
-router.get('/:listId', verifyToken, async (req, res) => {
-  try {
-    const list = await List.findById(req.params.listId).populate("owner");
-    res.status(200).json(list);
-  } catch (err) {
-    res.status(500).json({ err: err.message });
-  }
-});
-
-
 
 //DELETE LIST
 router.delete("/:listId", verifyToken, async (req, res) => {
